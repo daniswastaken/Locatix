a12e400c13bfb0583f4ee45190132e8356a25f61
daniswastaken <daniswastaken@gmail.com>
2025-09-04 09:55:32 +0700

    updating now using API instead of command block

---
+++ Locatix BP/scripts/main.js
@@ -0,0 +1,19 @@
+import { system, world } from "@minecraft/server";
+
+system.runInterval(() => {
+    world.getPlayers().forEach(p => {
+        const coords = { x: Math.floor(p.location.x), y: Math.floor(p.location.y), z: Math.floor(p.location.z) };
+
+
+        const hud = p.onScreenDisplay;
+        const ss = p.selectedSlotIndex;
+        const heldItem = p.getComponent("inventory").container.getItem(ss);
+
+
+        if (heldItem && heldItem.typeId === "locatix:coordinates") {
+            hud.setActionBar(`Coords: ${coords.x}, ${coords.y}, ${coords.z}`);
+
+        }
+    }
+    )
+});
